257145d7ddf63672f96e183b60c4ee907717c8d6
ratay <38402094+remziatay@users.noreply.github.com>
2020-09-22 21:05:21 +0200

    Updates

---
+++ precache-manifest.5af76dc64f792fc0a7b4c030baebbdd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143dfd6cc33183024c6bccd0602399ca",
+    "url": "/card-game-engine/index.html"
+  },
+  {
+    "revision": "f5f8690e25759ee7e3b1",
+    "url": "/card-game-engine/static/css/main.5bc027a4.chunk.css"
+  },
+  {
+    "revision": "6ef5327631d8bc2a37c5",
+    "url": "/card-game-engine/static/js/2.68211e71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-game-engine/static/js/2.68211e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f8690e25759ee7e3b1",
+    "url": "/card-game-engine/static/js/main.7a961d50.chunk.js"
+  },
+  {
+    "revision": "45f1f145cf279279601a",
+    "url": "/card-game-engine/static/js/runtime-main.f76411d4.js"
+  }
+]);
