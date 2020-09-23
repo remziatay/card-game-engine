4a5a2f8282874a9c509bf2ea341a2262e7dc0e94
ratay <38402094+remziatay@users.noreply.github.com>
2020-09-23 19:01:34 +0200

    added simple card picking behaviour

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,16 +4,17 @@ import styles from './Card.module.css'
 class Card extends React.Component {
   state = {
     center: 0,
-    focused: false
+    focused: false,
+    picked: false
   }
 
-  ref = React.createRef()
+  capturePointer = evt => { if (evt.buttons === 1) evt.target.setPointerCapture(evt.pointerId) }
+  releasePointer = evt => evt.target.releasePointerCapture(evt.pointerId)
 
-  focus = () => {
-    const rect = this.ref.current.getBoundingClientRect()
+  focus = evt => {
     this.setState({
       focused: true,
-      center: rect.left + rect.width / 2
+      center: evt.pageX
     })
   }
 
@@ -21,28 +22,66 @@ class Card extends React.Component {
     this.setState({ focused: false })
   }
 
+  startDrag = () => {
+    this.dragStarted = true
+  }
+
+  drag = evt => {
+    if (!this.dragStarted) return
+    if (!this.dragging) {
+      this.dragging = true
+      this.setState({ picked: true, focused: false })
+    }
+    this.setState({
+      x: evt.pageX - 2.5 * this.props.cardWidth / 2,
+      y: evt.pageY - 2.5 * this.props.style.height / 2
+    })
+  }
+
+  endDrag = () => {
+    this.dragStarted = false
+    this.dragging = false
+    this.setState({ picked: false })
+  }
+
   render () {
-    const style = { }
+    let style = { }
     if (this.state.focused) {
-      Object.assign(style, {
-        position: 'absolute',
-        left: this.state.center - parseFloat(this.props.cardWidth) / 2,
-        width: this.props.cardWidth,
-        transformOrigin: 'center',
-        transform: 'scale(120%) translateY(-60px)',
-        transition: 'none',
-        zIndex: 2,
-        pointerEvents: 'none'
-      })
+      style = {
+        left: this.state.center - 2.5 * this.props.cardWidth / 2,
+        bottom: '20%'
+      }
+    } else if (this.state.picked) {
+      style = {
+        position: 'fixed',
+        left: this.state.x,
+        top: this.state.y
+      }
     }
     return (
       <>
-        <div ref={this.ref} onMouseEnter={this.focus} onMouseLeave={this.unfocus} className={styles.Card} style={{ ...this.props.style, opacity: this.state.focused ? 0 : 1 }}>
+        <div role='button' tabIndex={0} onPointerDown={this.capturePointer}
+          onPointerUp={this.releasePointer} onMouseDown={this.startDrag}
+          onMouseMove={this.drag} onMouseUp={this.endDrag} onMouseEnter={this.focus}
+          onMouseLeave={this.unfocus} className={styles.Card}
+          style={{
+            ...this.props.style,
+            opacity: (this.state.focused || this.state.picked) ? 0 : 1,
+            display: this.state.picked ? 'none' : ''
+          }}>
           {this.props.num}. Card
         </div>
-        {this.state.focused &&
-        <div className={styles.Card} style={style}>
-          Focused {this.props.num}. Card
+
+        {(this.state.focused || this.state.picked) &&
+        <div className={styles.Card} style={{
+          position: 'absolute',
+          width: 2.5 * this.props.cardWidth + 'px',
+          height: 2.5 * this.props.style.height + 'px',
+          zIndex: 2,
+          pointerEvents: 'none',
+          ...style
+        }}>
+          {this.state.focused ? 'Focused' : 'Picked'} {this.props.num}. Card
         </div>
         }
       </>
